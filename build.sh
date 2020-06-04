c4d89554b439e0d7a69dcac45259eceda23ac6ca
Stephan Strittmatter <stephan@st-strittmatter.name>
2020-06-04 15:34:05 +0200

    remove obsolete calls

--- build.sh
+++ build.sh
@@ -1,12 +1,5 @@
 #!/usr/bin/env bash
-set -euo pipefail
 
-echo "Manually building webpage"
-
-python3 -m venv dependencies
-source dependencies/bin/activate
-pip install -r requirements.txt
-./grabrepos.py
 hugo
 
-echo "Webpage available at ./site"
+echo "Webpage available at ./public"
